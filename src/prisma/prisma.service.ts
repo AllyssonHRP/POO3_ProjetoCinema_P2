17c8c54c27253d127dd60d6c16823f6bf59c00ec
AllyssonHRP <allyssonroque123@gmail.com>
2026-06-09 17:13:54 -0300

    adição dos collections

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -5,12 +5,10 @@ import { PrismaLibSql } from '@prisma/adapter-libsql';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor() {
-    // No Prisma 7, passamos o objeto de configuração com a propriedade 'url' direto no construtor do adaptador
     const adapter = new PrismaLibSql({
       url: 'file:./prisma/dev.db',
     });
 
-    // Repassa o adaptador configurado para a inicialização do motor pai
     super({ adapter });
   }
 
